f3d834bf8a80798d22e062b7d5eddc1c03758530
Lucas Eduardo Pereira <lucaseduardodev@outlook.com>
2023-12-05 02:41:37 -0300

    feat: findByName method

--- src/main/java/com/lucas/company/repository/DepartmentRepository.java
+++ src/main/java/com/lucas/company/repository/DepartmentRepository.java
@@ -4,8 +4,10 @@ import com.lucas.company.model.Department;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface DepartmentRepository extends JpaRepository<Department, UUID> {
+    Optional<Department> findByName(String name);
 }
